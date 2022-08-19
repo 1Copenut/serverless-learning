8a81962039a92bac05228d1b3d5f044f6c6069d9
1Copenut <pierce.trevor@gmail.com>
2022-08-19 14:28:23 -0500

    Making a legit request to Unsplash API.

--- serverless-api/src/index.js
+++ serverless-api/src/index.js
@@ -17,6 +17,18 @@ addEventListener('fetch', event => {
  * @param {Request} request
  */
 async function handleRequest(request) {
-  const { query } = await request.json();
-  return new Response(`Your query was ${query}`);
+  const resp = await fetch('https://api.unsplash.com/photos', {
+    headers: {
+      Authorization: `Client-ID ${ACCESS_KEY}`
+    },
+  });
+  const data = await resp.json();
+  return new Response(JSON.stringify(data), {
+    headers: {
+      'Content-type': 'application/json'
+    },
+  });
+
+  // const { query } = await request.json()p;
+  // return new Response(`Your query was ${query}`);
 }
